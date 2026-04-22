8366bb4928962d64d06c5b54fc87b8b60a3b8db7
David Pachinik <ing.pachinik96@gmail.com>
2026-04-22 16:30:02 -0300

    feat(auth): configurar autenticación con NextAuth, Prisma y MongoDB
    - Integración de NextAuth con Google
    - Configuración de Prisma ORM
    - Conexión a MongoDB
    - Setup de adapter y sesiones

---
+++ src/lib/auth.ts
@@ -0,0 +1,25 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import GoogleProvider from 'next-auth/providers/google'
+import { AuthOptions } from 'next-auth'
+import PrismaClient from './prisma'
+
+export const authOptions:AuthOptions = {
+    adapter: PrismaAdapter(PrismaClient),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ],
+    callbacks:{
+        async session({session, token, user}){
+            session.user ={...session.user, id: user.id} as {
+                email:string;
+                name:string;
+                id:string,
+            }
+            return session
+        }
+
+    }
+}
