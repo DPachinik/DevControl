f3fd15e389d1d206fcdd7c9539314245686871c8
David Pachinik <ing.pachinik96@gmail.com>
2026-05-01 09:45:24 -0300

    feat(customer): implement customer form with validation and API integration

--- src/app/dashboard/customer/components/form/index.tsx
+++ src/app/dashboard/customer/components/form/index.tsx
@@ -1,7 +1,11 @@
 'use client'
+import { Input } from '@/components/input'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm} from 'react-hook-form'
 import {z} from 'zod'
+import {api} from '@/lib/api'
+import {useRouter} from 'next/navigation'
+
 
 const schema = z.object({
     name: z.string().trim().min(1,'El nombre es obligatorio'),
@@ -11,25 +15,83 @@ const schema = z.object({
         },{
         message:'El número debe estar en formato internacional: +595981123456'
     }),
-    adress:z.string(),
+    address:z.string(),
 })
 
 type FormData = z.infer<typeof schema>
 
-export function NewCustomerForm(){
+export function NewCustomerForm({userId}:{userId:string}){
 
     const {register, handleSubmit, formState: {errors}} = useForm<FormData>({
         resolver: zodResolver(schema)
     })
 
+    const router = useRouter();
+
+
+    async function handleRegister(data:FormData) {
+        await api.post('/api/customer', {
+            name:data.name,
+            email:data.email,
+            phone:data.phone,
+            address:data.address,
+            userId: userId
+        })
+
+        
+        router.replace('/dashboard/customer')
+    }
 
     return(
-        <form>
-            <label> Nombre del Cliente</label>
-            <input 
-            type="text"
-            placeholder="Luis"
+        <form className='flex flex-col mt-6' onSubmit={handleSubmit(handleRegister)}>
+
+            <label className='mb-1 text-lg font-medium'> Nombre completo</label>
+            <Input 
+             type='text'
+             name='name'
+             placeholder='Juan González'
+             register={register}
+             error={errors.name?.message}
             />
+
+            <section className='w-full flex flex-col sm:flex-row gap-4'>
+                <div className='flex-1'>
+                    <label className='mb-1 text-lg font-medium'>Email</label>
+                    <Input 
+                    type='emai;'
+                    name='email'
+                    placeholder='juan@gonzalez.com'
+                    register={register}
+                    error={errors.email?.message}
+                    />   
+                </div>
+
+                <div className='flex-1'>
+                    <label className='mb-1 text-lg font-medium'>Teléfono</label>
+                    <Input 
+                    type='text'
+                    name='phone'
+                    placeholder='+595981123456'
+                    register={register}
+                    error={errors.phone?.message}
+                    />  
+                </div>
+            </section>
+
+            <label className='mb-1 text-lg font-medium'>Ubicación</label>
+            <Input 
+             type='text'
+             name='address'
+             placeholder='Encarnación, Itapúa'
+             register={register}
+             error={errors.address?.message}
+            />
+
+            <button 
+            type='submit' 
+            className='bg-blue-500 my-4 px-2 h-11 rounded font-bold text-white'>
+                Registrar
+            </button>
         </form>
     )
 }
