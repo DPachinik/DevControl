052657f47bcb5363d5a1117a61ee6a153762b0da
David Pachinik <ing.pachinik96@gmail.com>
2026-05-01 10:03:01 -0300

    feat: update CustomerCard to use props and handle customer deletion via API

--- src/app/dashboard/customer/components/card/index.tsx
+++ src/app/dashboard/customer/components/card/index.tsx
@@ -1,11 +1,32 @@
+'use client'
+import { api } from "@/lib/api"
+import { CustomersProps } from "@/utils/customer.types"
+import { useRouter } from 'next/navigation'
 
-export function CustomerCard(){
+
+export function CustomerCard({customer}:{customer: CustomersProps}){
+    
+    const router = useRouter();
+
+    async function handleDelete(clientId:string){
+        try{
+            const response = await api.delete('/api/customer',{
+                params:{
+                    id:clientId
+                }
+            })
+            router.refresh()      
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <article className="flex flex-col bg-gray-100  border-2 border-slate-200 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
-           <h2> <span  className="font-medium">Nombre:</span> David Pachinik</h2>
-           <p><span  className="font-medium">Email:</span> pachi@gmail.com</p>
-           <p><span  className="font-medium">Teléfono:</span> 0992-988397</p>
-           <button className="bg-red-500 text-white px-2  rounded self-start mt-2">
+           <h2> <span  className="font-medium">Nombre: </span>{customer.name}</h2>
+           <p><span  className="font-medium">Email: </span>{customer.email}</p>
+           <p><span  className="font-medium">Teléfono: </span>{customer.phone}</p>
+           <button className="bg-red-500 text-white px-2  rounded self-start mt-2" onClick={()=>handleDelete(customer.id)}>
                 Eliminar
            </button>
         </article>
